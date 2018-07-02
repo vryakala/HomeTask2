0ccf4e1aefca77cfcf1b02a9c8ede0085c22f7d5
vishalryakala <vishal_ryakala@epam.com>
2018-07-02 09:44:33 +0530

    Variables demo

---
+++ FloatDemo.java
@@ -0,0 +1,12 @@
+package variablesdemo;
+
+public class FloatDemo {
+	public static void main(String[] args) {
+
+		float Daniel = 3.5f;
+		float Amber = 2.5f;
+		float Total = Daniel + Amber;
+		System.out.println("Total Number of apples = "+Total);
+
+	}
+}
---
+++ IntegersDemo.java
@@ -0,0 +1,15 @@
+package variablesdemo;
+
+public class IntegersDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int Daniel = 3;
+		int Amber = 2;
+		int Total = Daniel + Amber;
+		System.out.println("Total Number of apples = " + Total);
+
+	}
+
+}
